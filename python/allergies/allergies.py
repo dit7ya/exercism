e9d359521d8197d8474fa744b8a1e47a1cd0754f
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-08 23:06:42 +0530

    Tests working for allergies.py

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,19 @@
 class Allergies(object):
 
     def __init__(self, score):
-        pass
+        self.score = score%256
+        self.items_dict = {'eggs':1, 'peanuts':2, 'shellfish': 4, 'strawberries': 8, 'tomatoes': 16, 'chocolate': 32, 'pollen': 64, 'cats': 128}
 
-    def is_allergic_to(self, item):
-        pass
-
+    def is_allergic_to(self, item): 
+        if self.score & self.items_dict[item] == self.items_dict[item]:
+            return True
+        else:
+            return False
+        
     @property
     def lst(self):
-        pass
+        list = []
+        for key in self.items_dict.keys():
+            if self.is_allergic_to(key):
+                list.append(key)
+        return list
