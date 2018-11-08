200ec7262fe04daae48ca7473de3f40a68359cd8
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-08 13:44:33 +0530

    First commit.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,21 +1,16 @@
 def hey(phrase):
 
-    last_non_space_character = None
-    for i in range(len(phrase)):
-        if not phrase[i].isspace():
-            last_non_space_character = phrase[i]
-
-    if last_non_space_character == '?':
+    phrase = phrase.strip()
+    
+    if not phrase or phrase.isspace():
+        return 'Fine. Be that way!'
+    elif phrase[-1] == '?':
         if phrase.isupper():
             return 'Calm down, I know what I\'m doing!'
         else:
             return 'Sure.'
-
     elif phrase.isupper():
         return 'Whoa, chill out!'
-
-    elif not phrase or phrase.isspace():
-        return 'Fine. Be that way!'
     else:
         return 'Whatever.'
 
