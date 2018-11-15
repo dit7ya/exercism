95008884abc25fb5df1ed662f38de8d40fce0176
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-15 16:44:27 +0530

    Refactored and reformatted using Black.

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,20 +1,24 @@
 class Allergies(object):
+    items_dict = {
+        "eggs": 1,
+        "peanuts": 2,
+        "shellfish": 4,
+        "strawberries": 8,
+        "tomatoes": 16,
+        "chocolate": 32,
+        "pollen": 64,
+        "cats": 128,
+    }
 
     def __init__(self, score):
         self.score = score % 256
-        self.items_dict = {'eggs': 1, 'peanuts': 2, 'shellfish': 4, 'strawberries': 8, 'tomatoes': 16, 'chocolate': 32,
-                           'pollen': 64, 'cats': 128}
+        self.allergen_list = [
+            key for key in self.items_dict.keys() if self.is_allergic_to(key)
+        ]
 
     def is_allergic_to(self, item):
-        if self.score & self.items_dict[item] == self.items_dict[item]:
-            return True
-        else:
-            return False
+        return self.score & self.items_dict[item] == self.items_dict[item]
 
     @property
     def lst(self):
-        allergen_list = []
-        for key in self.items_dict.keys():
-            if self.is_allergic_to(key):
-                allergen_list.append(key)
-        return allergen_list
+        return self.allergen_list
