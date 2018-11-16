1fab6e2e165e87c4327ac090581ca81ed6700bad
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-16 12:55:33 +0530

    First try of sum-of-multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,13 @@
+import numpy as np
 def sum_of_multiples(limit, multiples):
-    pass
+    muls = []
+    for num in multiples:
+        for i in range(limit):
+            if i%num == 0:
+                muls.append(i)
+    sum = np.sum(np.unique(muls))
+    return sum
+
+
+
+
