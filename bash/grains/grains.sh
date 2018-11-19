85c3c8ab2edce6354875dbae83d8082335601924
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-19 16:52:05 +0530

    Working solution.

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -4,17 +4,17 @@
 # This option will make the script exit when there is an error
 #set -o errexit
 
-
-to=$(echo 2^64 - 1 | bc)
-
 if [ "$1" = "total" ]
 then
+    to=$(echo 2^64 - 1 | bc)
     echo "$to"
+
+elif [ "$1" -gt "64" ] || [ "$1" -le "0" ]
+then
+    echo "Error: invalid input"
+    exit 1
+
 else
-    #((grains=2**($1-1)))
-    grains=$(echo 2^$(echo $1 - 1 | bc) | bc)
+    grains=$(echo "(2^($1 - 1))" | bc)
     echo "$grains"
 fi
-
-:+1:
-:es:
