fc12b72da9a63074e1a499c9214615d3928cb689
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-11-23 12:48:13 +0530

    Delete __pycache__

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,16 +1,11 @@
 class Garden(object):
+    _plants_dict = {'V': 'Violets', 'R': 'Radishes', 'C': 'Clover', 'G': 'Grass'}
     def __init__(self, diagram, students= "Alice  Bob  Charlie  David  Eve  Fred  Ginny  Harriet  Ileana  Joseph  Kincaid  Larry".split()):
-        self.diagram = diagram
-        self.students = students
+        self.diagram = diagram.split()
+        self.students = sorted(students)
         self._plants = {}
-        self._plants_dict = {'V': 'Violets', 'R': 'Radishes', 'C': 'Clover', 'G': 'Grass'}
-        self._rows = diagram.split()
         for idx in range(len(self.students)):
-            self._plants[self.students[idx]] = self._rows[0][idx*2:idx*2 + 2] + self._rows[1][idx*2:idx*2 + 2]
+            self._plants[self.students[idx]] = self.diagram[0][idx*2:idx*2 + 2] + self.diagram[1][idx*2:idx*2 + 2]
     def plants(self, student):
-        plants = []
-        plants_short = self._plants[student]
-        for i in plants_short:
-            plants.append(self._plants_dict[i])
-
+        plants = [self._plants_dict[i] for i in self._plants[student]]
         return plants
