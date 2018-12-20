5c0bceb7f976e16f9a566c435ee0a4fbcf489cd8
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-12-20 11:32:56 +0530

    Working solution for two-fer.py

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,9 +1,4 @@
-import numpy as np
 def is_armstrong(number):
     num_digit = len(str(number))
     digits = [int(x) for x in str(number)]
-    sum = np.sum([x**num_digit for x in digits])
-    ans = False
-    if number==sum:
-        ans = True
-    return ans
+    return number == sum([x**num_digit for x in digits])
