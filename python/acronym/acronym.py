b53b50f04607388795defb0503f30469d3fc90d9
dit7a <sayan.sarkar@students.iiserpune.ac.in>
2018-12-29 12:20:50 +0530

    Working solution for acronym.py

---
+++ python/acronym/acronym.py
@@ -0,0 +1,10 @@
+def abbreviate(words):
+
+    string_list = words.split('-')      # split by hyphens
+    words = [string.split() for string in string_list]  # converts the pieces of strings to words by splitting with space
+    words = [item for sublist in words for item in sublist]      # flattens list
+
+    acr = ''.join([word[0].upper() for word in words])
+
+
+    return acr
