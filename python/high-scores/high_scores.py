8a0de9b00df232e9f47280cbcf792db3fd2f93dd
dit7ya <7rat13@gmail.com>
2019-01-15 11:01:58 +0530

    Formarring changes.

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -2,20 +2,22 @@ class HighScores(object):
     def __init__(self, scores):
         self.scores = scores
 
-    def scores(self):
-        return self.scores
     def latest(self):
         return self.scores[-1]
+
     def personal_best(self):
         return max(self.scores)
+
     def personal_top(self):
-        return sorted(self.scores, reverse = True)[:3]
+        return sorted(self.scores, reverse=True)[:3]
+
     def report(self):
         l = self.latest()
         p = self.personal_best()
         diff = p - l
         if l == p:
-            return 'Your latest score was {0}. That\'s your personal best!'.format(l)
+            return "Your latest score was {0}. That's your personal best!".format(l)
         else:
-            return 'Your latest score was {0}. That\'s {1} short of your personal best!'.format(l,diff)
-
+            return "Your latest score was {0}. That's {1} short of your personal best!".format(
+                l, diff
+            )
