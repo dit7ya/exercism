eaccbcce211739dfd90ae397d177f62debaf1f8f
dit7ya <7rat13@gmail.com>
2019-01-26 15:57:08 +0530

    Slight refactoring).

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -3,6 +3,6 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Both strands should be of equal length.")
 
-    count = sum((x[0] != x[1] for x in list(zip(strand_a, strand_b))))
+    count = sum((x[0] != x[1] for x in zip(strand_a, strand_b)))
 
     return count
